5393079e3e7de046e2e8a6260424caeb84b83f13
李嘉俊 <2383690354@qq.com>
2024-07-17 01:45:15 +0800

    添加数据中心

---
+++ iOSClub.UniversityLink/iOSClub.UniversityLink/Models/AllDataModel.cs
@@ -0,0 +1,9 @@
+﻿using UniversityLink.DataModels;
+
+namespace iOSClub.UniversityLink.Models;
+
+public class AllDataModel
+{
+    public List<UserModel> Users { get; set; } = [];
+    public List<CategoryModel> Categories { get; set; } = [];
+}
---
+++ iOSClub.UniversityLink/iOSClub.UniversityLink/wwwroot/site.js
@@ -0,0 +1,8 @@
+function jsSaveAsFile(filename, byteBase64) {
+    const link = document.createElement('a');
+    link.download = filename;
+    link.href = "data:application/octet-stream;base64," + byteBase64;
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
