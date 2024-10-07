7767df261a8a497033e30413182cc0020ab50a25
李嘉俊 <2383690354@qq.com>
2024-10-07 14:16:50 +0800

    更新UI

--- iOSClub.UniversityLink/iOSClub.UniversityLink/Controllers/LinkController.cs
+++ iOSClub.UniversityLink/iOSClub.UniversityLink/Controllers/LinkController.cs
@@ -1,10 +1,11 @@
+using System.Text;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using UniversityLink.DataModels;
 
 namespace iOSClub.UniversityLink.Controllers;
 
-[Route("api/[controller]")]
+[Route("api/[controller]/[action]")]
 [ApiController]
 public class LinkController(LinkContext context) : ControllerBase
 {
@@ -17,9 +18,34 @@ public class LinkController(LinkContext context) : ControllerBase
             .ToListAsync();
     }
     
-    [HttpGet("/Link")]
+    [HttpGet]
     public async Task<ActionResult<List<LinkModel>>> GetLink()
     {
         return await context.Links.ToListAsync();
+    }
+    
+    [HttpGet]
+    public async Task<ActionResult<string>> GetMd()
+    {
+        var c = await context.Categories
+            .Include(x => x.Links.OrderBy(y => y.Index))
+            .OrderBy(x => x.Index)
+            .ToListAsync();
+        
+        var builder = new StringBuilder();
+        foreach (var model in c)
+        {
+            builder.AppendLine($"## {model.Name}");
+            builder.AppendLine("");
+            builder.AppendLine(model.Description);
+            builder.AppendLine("");
+            foreach (var link in model.Links)
+            {
+                builder.AppendLine($"- [{link.Name}]({link.Url} \"{link.Description}\")");
+            }
+            builder.AppendLine("");
+        }
+        
+        return builder.ToString();
     }
 }
